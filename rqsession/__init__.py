c8d2fdfdf2236fe6e2cb55b2ffc3a309d625cab7
sherlock <zhzhsgg@gmail.com>
2026-06-12 18:20:44 +0800

    chore: bump version to 0.4.7

--- rqsession/__init__.py
+++ rqsession/__init__.py
@@ -9,7 +9,7 @@ Legacy API (pure Python):
     from rqsession import RequestSession, EnhancedRequestSession
 """
 
-__version__ = "0.4.6"
+__version__ = "0.4.7"
 __author__ = "Sherlock"
 __email__ = "zhzhsgg@gmail.com"
 
